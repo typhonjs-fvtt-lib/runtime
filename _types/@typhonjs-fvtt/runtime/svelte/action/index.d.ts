93f668338342e0b93dfa7ba3a5747d7f8c4ca4b3
typhonrt <support@typhonjs.io>
2021-11-25 12:49:55 -0800

    Types test / added _types

---
+++ _types/@typhonjs-fvtt/runtime/svelte/action/index.d.ts
@@ -0,0 +1,23 @@
+/**
+ * Provides an action to enable pointer dragging of an HTMLElement and invoke `setPosition` on given Positionable
+ * object provided. When the attached boolean store state changes the draggable action is enabled or disabled.
+ *
+ * @param {HTMLElement}       node - The node associated with the action.
+ *
+ * @param {object}            params - Required parameters.
+ *
+ * @param {Positionable}      params.positionable - A positionable object.
+ *
+ * @param {Readable<boolean>} params.booleanStore - A Svelte store that contains a boolean.
+ *
+ * @returns {{update: Function, destroy: Function}} The action lifecycle methods.
+ */
+declare function draggable(node: HTMLElement, { positionable, booleanStore }: {
+    positionable: any;
+    booleanStore: any;
+}): {
+    update: Function;
+    destroy: Function;
+};
+
+export { draggable };
---
+++ _types/@typhonjs-fvtt/runtime/svelte/store/index.d.ts
@@ -0,0 +1,156 @@
+import * as svelte_store from 'svelte/store';
+import { noop } from 'svelte/types/runtime/internal/utils';
+import { get } from 'svelte/types/runtime/store';
+
+/**
+ * - The backing Svelte store; a writable w/ get method attached.
+ */
+type LSStore = ((key: any, value: any, start?: typeof noop) => {
+    set: (new_value: any) => void;
+    update: (fn: any) => void;
+    subscribe: (run: any, invalidate?: typeof noop) => svelte_store.Unsubscriber;
+}) & typeof get;
+/**
+ * - The backing Svelte store; a writable w/ get method attached.
+ */
+type SSStore = ((key: any, value: any, start?: typeof noop) => {
+    set: (new_value: any) => void;
+    update: (fn: any) => void;
+    subscribe: (run: any, invalidate?: typeof noop) => svelte_store.Unsubscriber;
+}) & typeof get;
+/**
+ * - Defines a game setting.
+ */
+type GameSetting = {
+    /**
+     * - The ID of the module / system.
+     */
+    moduleId: string;
+    /**
+     * - The setting key to register.
+     */
+    key: string;
+    /**
+     * - Configuration for setting data.
+     */
+    options: object;
+};
+/**
+ * - The backing Svelte store; a writable w/ get method attached.
+ */
+type GSStore = ((key: any, value: any, start?: typeof noop) => {
+    set: (new_value: any) => void;
+    update: (fn: any) => void;
+    subscribe: (run: any, invalidate?: typeof noop) => svelte_store.Unsubscriber;
+}) & typeof get;
+declare class LocalStorage {
+    /**
+     * Get value from the localstorage.
+     *
+     * @param {string}   key - Key to lookup in localstorage.
+     *
+     * @param {*}        [defaultValue] - A default value to return if key not present in local storage.
+     *
+     * @returns {*} Value from local storage or if not defined any default value provided.
+     */
+    getItem(key: string, defaultValue?: any): any;
+    /**
+     * Returns the backing Svelte store for the given key; potentially sets a default value if the key
+     * is not already set.
+     *
+     * @param {string}   key - Key to lookup in localstorage.
+     *
+     * @param {*}        [defaultValue] - A default value to return if key not present in local storage.
+     *
+     * @returns {LSStore} The Svelte store for this key.
+     */
+    getStore(key: string, defaultValue?: any): LSStore;
+    /**
+     * Sets the value for the given key in localstorage.
+     *
+     * @param {string}   key - Key to lookup in localstorage.
+     *
+     * @param {*}        value - A value to set for this key.
+     */
+    setItem(key: string, value: any): void;
+    /**
+     * Convenience method to swap a boolean value stored in local storage.
+     *
+     * @param {string}   key - Key to lookup in localstorage.
+     *
+     * @param {boolean}  [defaultValue] - A default value to return if key not present in local storage.
+     *
+     * @returns {boolean} The boolean swap for the given key.
+     */
+    swapItemBoolean(key: string, defaultValue?: boolean): boolean;
+}
+declare class SessionStorage {
+    /**
+     * Get value from the sessionstorage.
+     *
+     * @param {string}   key - Key to lookup in sessionstorage.
+     *
+     * @param {*}        [defaultValue] - A default value to return if key not present in session storage.
+     *
+     * @returns {*} Value from session storage or if not defined any default value provided.
+     */
+    getItem(key: string, defaultValue?: any): any;
+    /**
+     * Returns the backing Svelte store for the given key; potentially sets a default value if the key
+     * is not already set.
+     *
+     * @param {string}   key - Key to lookup in sessionstorage.
+     *
+     * @param {*}        [defaultValue] - A default value to return if key not present in session storage.
+     *
+     * @returns {LSStore} The Svelte store for this key.
+     */
+    getStore(key: string, defaultValue?: any): LSStore;
+    /**
+     * Sets the value for the given key in sessionstorage.
+     *
+     * @param {string}   key - Key to lookup in sessionstorage.
+     *
+     * @param {*}        value - A value to set for this key.
+     */
+    setItem(key: string, value: any): void;
+    /**
+     * Convenience method to swap a boolean value stored in session storage.
+     *
+     * @param {string}   key - Key to lookup in sessionstorage.
+     *
+     * @param {boolean}  [defaultValue] - A default value to return if key not present in session storage.
+     *
+     * @returns {boolean} The boolean swap for the given key.
+     */
+    swapItemBoolean(key: string, defaultValue?: boolean): boolean;
+}
+/**
+ * @typedef {object} GameSetting - Defines a game setting.
+ *
+ * @property {string} moduleId - The ID of the module / system.
+ *
+ * @property {string} key - The setting key to register.
+ *
+ * @property {object} options - Configuration for setting data.
+ */
+/**
+ * @typedef {writable & get} GSStore - The backing Svelte store; a writable w/ get method attached.
+ */
+/**
+ * Registers game settings and creates a backing Svelte store for each setting. It is possible to add multiple
+ * `onChange` callbacks on registration.
+ */
+declare class TJSGameSettings {
+    getStore(key: any): GSStore;
+    register(moduleId: any, key: any, options?: {}): void;
+    /**
+     * Registers multiple settings.
+     *
+     * @param {Iterable<GameSetting>} settings - An iterable list of game setting configurations to register.
+     */
+    registerAll(settings: Iterable<GameSetting>): void;
+    #private;
+}
+
+export { GSStore, GameSetting, LSStore, LocalStorage, SSStore, SessionStorage, TJSGameSettings };
