778298686944ee26d2e2d17e705ecbb4eb28f621
typhonrt <support@typhonjs.io>
2022-10-03 17:09:16 -0700

    Update oEmbed TinyMCE plugin

--- .build/tinymce/initializePlugins.js
+++ .build/tinymce/initializePlugins.js
@@ -8,6 +8,34 @@ export function initializePlugins()
    if (!initialized)
    {
       initialized = true;
-      import('./oembed.js');
+
+      /**
+       * Handle loading the TyphonJS oEmbed TinyMCE plugin based on Foundry version.
+       *
+       * v9 of Foundry ships with TinyMCE v5.
+       * v10 of Foundry ships with TinyMCE v6.
+       */
+      Hooks.once('init', async () =>
+      {
+         const isV10 = !foundry.utils.isNewerVersion(10, game.version ?? game?.data?.version);
+
+         try
+         {
+            if (isV10)
+            {
+               // Load oEmbed TinyMCE v6 plugin.
+               await import('./typhonjs-oembed-v6.js');
+            }
+            else
+            {
+               // Load oEmbed TinyMCE v5 plugin.
+               await import('./typhonjs-oembed-v5.js');
+            }
+         }
+         catch (err)
+         {
+            console.warn(`TyphonJS Runtime Library warning: Failed to load TyphonJS oEmbed plugin.`);
+         }
+      });
    }
 }
--- .build/tinymce/oembed.js
+++
@@ -1,1 +0,0 @@
-import '@typhonjs-tinymce/oembed';
