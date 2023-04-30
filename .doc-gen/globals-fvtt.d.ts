a95e9b8f60a925bcc7ac9217266ac7f174fdefbe
typhonrt <support@typhonjs.io>
2023-04-30 04:02:06 -0700

    Temporary configuration for typedoc.

---
+++ .doc-gen/globals-fvtt.d.ts
@@ -0,0 +1,12 @@
+declare global {
+   export type ApplicationHeaderButton = object;
+   export type DocumentCollection = object;
+   export type game = object;
+   export namespace foundry {
+      namespace abstract {
+         export type Document = object;
+      }
+   }
+}
+
+export {};
